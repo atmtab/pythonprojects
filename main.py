5742be0005d33c9dbcca3febd7290018aacbbc61
atmtab <4tm1ive@gmail.com>
2023-06-24 17:10:13 +0500

    hello

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello world')
+access = False
+while not access:
+    username = input('Enter your username: ')
+    password = input('Enter your password: ')
+    if username == 'Kuma' and password == '12345678':
+        print('Access granted')
+        access = True
+    elif username == 'Kuma':
+        print('Access denied, Wrong password')
+    else:
+        print('Access denied, Wrong username')
+print('End of program')
